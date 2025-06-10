5f8535da14414b0a9f522a8d50041de91253c93f
Chong Shik Park <cspark7701@gmail.com>
2025-06-10 13:45:34 +0900

    Switch python environment from conda to pyenv. Current development python version is 3.11.9

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-conda activate synapticTRACK
+# pyenv environment
+export PATH="$HOME/.pyenv/bin:/opt/julia-1.10.4/bin/:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# linking dynamic libpython3.11
+export LD_LIBRARY_PATH=$HOME/.pyenv/versions/3.11.9/lib:$LD_LIBRARY_PATH
+
+# deprecated: previos python environment with conda
+#conda activate synapticTRACK
+
+
