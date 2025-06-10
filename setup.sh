1ece17500a83fa6fbd114289fc0d6c613d538469
Chong Shik Park <cspark7701@gmail.com>
2025-06-10 14:13:06 +0900

    add env activation line

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ export PATH="$HOME/.pyenv/bin:/opt/julia-1.10.4/bin/:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+pyenv activate synapticTrack
 
 # linking dynamic libpython3.11
 export LD_LIBRARY_PATH=$HOME/.pyenv/versions/3.11.9/lib:$LD_LIBRARY_PATH
