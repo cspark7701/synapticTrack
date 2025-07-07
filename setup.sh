df5068d328c171308df57c315b37e8437a6c148e
Chong Shik Park <cspark7701@gmail.com>
2025-07-07 16:45:16 +0900

    update environment setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # pyenv environment
-export PATH="$HOME/.pyenv/bin:/opt/julia-1.10.4/bin/:$PATH"
+export PATH="$HOME/.pyenv/bin:$HOME/Work/simulation_codes-working/julia/bin/:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv activate synapticTrack
 
-# linking dynamic libpython3.11
-export LD_LIBRARY_PATH=$HOME/.pyenv/versions/3.11.9/lib:$LD_LIBRARY_PATH
-
-# deprecated: previos python environment with conda
-#conda activate synapticTRACK
-
-
+# linking dynamic libpython3.14
+if [ -n "${PYTHONPATH}" ]
+then
+    export LD_LIBRARY_PATH="$HOME/.pyenv/versions/3.13.5/lib:$LD_LIBRARY_PATH"
+else
+    export LD_LIBRARY_PATH=$HOME/.pyenv/versions/3.13.5/lib
+fi
